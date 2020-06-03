2224b84e97442a24881fc7baab2142279e0e65ab
JesusCH92 <jcocaharo@hotmail.com>
2020-06-03 22:51:56 +0200

    add twig in BaseController for render templates

--- www/html/test-app/src/BaseController.php
+++ www/html/test-app/src/BaseController.php
@@ -2,13 +2,20 @@
 
 namespace TestApp;
 
+use Twig\Loader\FilesystemLoader;
 class BaseController
 {
     /** @var array */
     protected $dc;
+    protected $templateEngine;
 
     public function __construct(array $dc)
     {
         $this->dc = $dc;
+        $loader = new FilesystemLoader(__DIR__ .'/../templates');
+        $this->templateEngine = new \Twig\Environment($loader, [
+            'debug' => true,
+            // 'cache' => 'false',
+        ]); 
     }
 }
