cb6946fa1a6fad7baedb1ae1349749c1a6b9972b
JesusCH92 <jcocaharo@hotmail.com>
2020-06-05 03:46:43 +0200

    implements LoadImages module, config dropzone for load images

--- www/html/test-app/assets/js/ModuleLoadImage.js
+++ www/html/test-app/assets/js/ModuleLoadImage.js
@@ -1,1 +1,42 @@
-console.log('load images by ajax');
+var LoadImagesModule = (function(){
+    var $clearPhotosBtn = $("#clear-photos-btn");
+    // var $dropzone = new Dropzone("#archivos", {
+    //     url: '/save_photos'
+    // });
+    // var $photos = $("#photos");
+    // var $photos = $("div#myId").dropzone({ url: "/file/post" });
+    console.log('load images by ajax');
+    Dropzone.options.dropzoneFrom = {
+        autoProcessQueue: false,
+        acceptedFiles:".png,.jpg,.gif,.bmp,.jpeg",
+        parallelUploads: 10,
+        init: function(){
+            var submitButton = document.querySelector('#clear-photos-btn');
+            myDropzone = this;
+            submitButton.addEventListener("click", function(){
+                myDropzone.processQueue();
+            });
+            this.on("complete", function($file){
+                console.log($file.xhr.response);
+                if (this.getQueuedFiles().length == 0 && this.getUploadingFiles().length == 0) {
+                    var _this = this;
+                    _this.removeAllFiles();
+                }
+                sendPhotoAndSaved();
+            });
+        },
+    };
+
+    var sendPhotoAndSaved = function(callback=console.log){
+        $.ajax({
+            type: 'POST',
+            url: '/save_photos',
+            async: true,
+            dataType: "json",
+            success: function(data){
+                callback(data);
+            }
+        });
+    };
+
+})()
