9452029407016ea059c32ccff1909e7b77896100
JesusCH92 <jcocaharo@hotmail.com>
2020-06-16 05:06:40 +0200

    implements productor with rabbitmq for all filtering consumers

--- www/html/test-app/src/SavePhotosController.php
+++ www/html/test-app/src/SavePhotosController.php
@@ -4,13 +4,14 @@ namespace TestApp;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use PhpAmqpLib\Message\AMQPMessage;
 
 class SavePhotosController extends BaseController
 {
     public function __invoke(Request $request)
     {
-        // var_dump( $request);
-        // var_dump($_FILES);
+        $rabbitmq = $this->dc['rabbitmq'];
+        $channel = $rabbitmq->channel();
 
         if(!$request->isXmlHttpRequest()){
             echo 'mierda';
@@ -29,11 +30,24 @@ class SavePhotosController extends BaseController
         if (!$templocation) {
             die('no ha seleccionado ningun archivo');
         } 
-        if (move_uploaded_file($templocation, "$routeFiles/$name")) {
-            echo 'archivo guardado correctamente';
-        } else{
+        if (!move_uploaded_file($templocation, "$routeFiles/$name")) {
             echo 'error al guardar archivo';
         }
+
+        foreach (self::IMAGESFILTERCONSUMERS as $imageFilterConsumer) {
+            var_dump($imageFilterConsumer);
+            $channel->queue_declare('imageFiler', false, false, false, false);
+
+            $msg = new AMQPMessage(json_encode([
+                'file_name' => $name,
+                'file_path' => $routeFiles
+            ]));
+            
+            $channel->basic_publish($msg, '', $imageFilterConsumer);
+        }
+
+        echo 'archivo guardado correctamente.' . PHP_EOL;
+
         return Response::create("hi", 200);
     }
 }
