3c4fac6dcab4d21422e08ae360f05e2bfea47da3
JesusCH92 <jcocaharo@hotmail.com>
2020-06-16 14:40:47 +0200

    try to edit image with filter

--- www/html/test-app/SepiaFilterConsumer.php
+++ www/html/test-app/SepiaFilterConsumer.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
+use function GuzzleHttp\json_decode;
+
 $rabbitmq = new AMQPStreamConnection('rabbitmq', 5672, 'rabbitmq', 'rabbitmq');
 $channel = $rabbitmq->channel();
 
@@ -13,6 +15,19 @@ echo 'Add filter to images ' . PHP_EOL;
 
 $callback = function ($msg) {
     echo ' [x] Add Filter sepia: ' . $msg->body . PHP_EOL;
+    $imageProperties = json_decode($msg->body);
+    // var_dump($imageProperties->file_path);
+    echo $imageProperties->file_path . '/' . $imageProperties->file_name . PHP_EOL;
+    // $im = imageCreateFromPng($imageProperties->file_path . '/' . $imageProperties->file_name);
+    // if ($im && imagefilter($im, IMG_FILTER_GRAYSCALE)) {
+    //     echo 'Imagen convertida a escala de grises.';
+
+    //     imagepng($im, 'dave.png');
+    // } else {
+    //     echo 'La conversión a escala de grises falló.';
+    // }
+
+    // imagedestroy($im);
 };
 
 $channel->basic_consume('sepia', '', false, true, false, false, $callback);
