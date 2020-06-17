c40dd4a70fe7f7ceb27facb0f76b1445d8bdbd73
JesusCH92 <jcocaharo@hotmail.com>
2020-06-17 11:43:05 +0200

    check if gd extension works-> it's worksgit add www/html/test-app/SepiaFilterConsumer.php

--- www/html/test-app/SepiaFilterConsumer.php
+++ www/html/test-app/SepiaFilterConsumer.php
@@ -12,13 +12,20 @@ $channel = $rabbitmq->channel();
 $channel->queue_declare('sepia', false, false, false, false);
 
 echo 'Add filter to images ' . PHP_EOL;
+var_dump(gd_info());
 
 $callback = function ($msg) {
     echo ' [x] Add Filter sepia: ' . $msg->body . PHP_EOL;
     $imageProperties = json_decode($msg->body);
     // var_dump($imageProperties->file_path);
-    echo $imageProperties->file_path . '/' . $imageProperties->file_name . PHP_EOL;
-    // $im = imageCreateFromPng($imageProperties->file_path . '/' . $imageProperties->file_name);
+    // $pathImage = $imageProperties->file_path . '/' . $imageProperties->file_name;
+    // $pathImage = '/www/html/test-app/assets/files/endpoint-http' . $imageProperties->file_name;
+    // echo $pathImage . PHP_EOL;
+
+    // $pathImg = __DIR__ . '\assets\files\endpoint-http.png';
+    // echo $pathImg . PHP_EOL;
+    // // $imgPath = '/www/html/test-app/assets/files/endpoint-http.png';
+    // $im = imagecreatefrompng ($pathImg);
     // if ($im && imagefilter($im, IMG_FILTER_GRAYSCALE)) {
     //     echo 'Imagen convertida a escala de grises.';
 
