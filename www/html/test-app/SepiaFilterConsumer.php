49f48034ce179b022b9ad7350daedfe3e063ffda
JesusCH92 <jcocaharo@hotmail.com>
2020-06-20 17:11:46 +0200

    add for edit images with sepia filter using claviska library

--- www/html/test-app/SepiaFilterConsumer.php
+++ www/html/test-app/SepiaFilterConsumer.php
@@ -3,6 +3,8 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
+use TestApp\ImagesFilter\ApplicationService\AddFilterImagesService;
+use TestApp\ImagesFilter\Infrastructure\FilterImageCreator;
 
 use function GuzzleHttp\json_decode;
 
@@ -12,28 +14,41 @@ $channel = $rabbitmq->channel();
 $channel->queue_declare('sepia', false, false, false, false);
 
 echo 'Add filter to images ' . PHP_EOL;
+echo 'Hello there!' . PHP_EOL;
+echo 'madein madein!!' . PHP_EOL;
 
 $callback = function ($msg) {
     echo ' [x] Add Filter sepia: ' . $msg->body . PHP_EOL;
     $imageProperties = json_decode($msg->body);
-    var_dump(json_encode($imageProperties->file_path));
-    // $pathImage = $imageProperties->file_path . '/' . $imageProperties->file_name;
-    // $pathImage = '/www/html/test-app/assets/files/endpoint-http' . $imageProperties->file_name;
-    // echo $pathImage . PHP_EOL;
+    var_dump($imageProperties);
 
-    $pathImg = __DIR__ . '/assets/files/';
-    echo $pathImg . PHP_EOL;
-    // // $imgPath = '/www/html/test-app/assets/files/endpoint-http.png';
-    $im = imagecreatefrompng ($pathImg .'endpoint-http.png');
-    if ($im && imagefilter($im, IMG_FILTER_GRAYSCALE)) {
-        echo 'Imagen convertida a escala de grises.' . PHP_EOL;
+    // $filterImageCreator = new FilterImageCreator();
+    // $addFilterImagesService = new AddFilterImagesService($filterImageCreator);
+    echo 'general kenobi!' . PHP_EOL;
 
-        imagepng($im, $pathImg . 'sepia-' .'endpoint-http.png');
-    } else {
-        echo 'La conversión a escala de grises falló.' . PHP_EOL;
+    // $addFilterImagesService->__invoke($imageProperties->file_path, $imageProperties->file_name, $imageProperties->file_extension, 'addSepiaFilter');
+    echo 'there is a new service for add filter to images!! ' . PHP_EOL;
+
+    // $imagePath = $imageProperties->file_path;
+    $imagePath = '/code/test-app/assets/files';
+    $imageName = $imageProperties->file_name;
+    $imageExtension = $imageProperties->file_extension;
+
+    echo "$imagePath/$imageName.$imageExtension" . PHP_EOL;
+
+    try {
+        $imageClaviskaSimpleImage = new \claviska\SimpleImage();
+        $imageClaviskaSimpleImage
+            ->fromFile("$imagePath/$imageName.$imageExtension")
+            ->sepia()
+            ->toFile("$imagePath/sepia-$imageName.$imageExtension", 'image/png')
+            ->toScreen();
+        echo 'it works? ' . PHP_EOL;
+    } catch (Exception $err) {
+        echo 'mierddddda ' . PHP_EOL;
+        // echo $err->getMessage();
     }
-
-    imagedestroy($im);
+    echo 'image create ' . PHP_EOL;
 };
 
 $channel->basic_consume('sepia', '', false, true, false, false, $callback);
