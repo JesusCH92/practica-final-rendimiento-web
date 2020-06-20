e782c09245ee69d084f6a74e0e96004af5b8512f
JesusCH92 <jcocaharo@hotmail.com>
2020-06-20 22:07:04 +0200

    add consumer for edit image with black and white filter

--- www/html/test-app/BlackAndWhiteFilterConsumer.php
+++ www/html/test-app/BlackAndWhiteFilterConsumer.php
@@ -3,6 +3,10 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
+use TestApp\ImagesFilter\ApplicationService\AddFilterImagesService;
+use TestApp\ImagesFilter\Infrastructure\FilterImageCreator;
+
+use function GuzzleHttp\json_decode;
 
 $rabbitmq = new AMQPStreamConnection('rabbitmq', 5672, 'rabbitmq', 'rabbitmq');
 $channel = $rabbitmq->channel();
@@ -10,9 +14,24 @@ $channel = $rabbitmq->channel();
 $channel->queue_declare('blackAndWhite', false, false, false, false);
 
 echo 'Add filter to images ' . PHP_EOL;
+echo 'Hello there!' . PHP_EOL;
 
-$callback = function ($msg) {
+$filterImageCreator = new FilterImageCreator();
+$addFilterImagesService = new AddFilterImagesService($filterImageCreator);
+
+$callback = function ($msg) use ($addFilterImagesService){
     echo ' [x] Add Filter black and white: ' . $msg->body . PHP_EOL;
+    $imageProperties = json_decode($msg->body);
+
+    echo 'general kenobi!' . PHP_EOL;
+
+    $imagePath = $imageProperties->file_path;
+    $imageName = $imageProperties->file_name;
+    $imageExtension = $imageProperties->file_extension;
+
+    $addFilterImagesService->__invoke($imagePath, $imageName, $imageExtension, 'addBlackAndWhiteFilter');
+
+    echo 'order 66 is execute!!!' . PHP_EOL;
 };
 
 $channel->basic_consume('blackAndWhite', '', false, true, false, false, $callback);
