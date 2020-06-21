2cf03e2b1650fd8afd17fe9dd2062d2af36c51df
JesusCH92 <jcocaharo@hotmail.com>
2020-06-21 14:34:58 +0200

    refactor for thorw exception if AddFilterImagesService failed

--- www/html/test-app/BlackAndWhiteFilterConsumer.php
+++ www/html/test-app/BlackAndWhiteFilterConsumer.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use TestApp\ImagesFilter\ApplicationService\AddFilterImagesService;
 use TestApp\ImagesFilter\Infrastructure\FilterImageCreator;
+use TestApp\Shared\Infrastructure\Exceptions\ExceptionClassToHumanMessageMapper;
 
 use function GuzzleHttp\json_decode;
 
@@ -13,25 +14,26 @@ $channel = $rabbitmq->channel();
 
 $channel->queue_declare('blackAndWhite', false, false, false, false);
 
-echo 'Add filter to images ' . PHP_EOL;
-echo 'Hello there!' . PHP_EOL;
+echo 'Add black and white filter to images ' . PHP_EOL;
 
 $filterImageCreator = new FilterImageCreator();
 $addFilterImagesService = new AddFilterImagesService($filterImageCreator);
 
 $callback = function ($msg) use ($addFilterImagesService){
-    echo ' [x] Add Filter black and white: ' . $msg->body . PHP_EOL;
-    $imageProperties = json_decode($msg->body);
+    try{
+        $imageProperties = json_decode($msg->body);
 
-    echo 'general kenobi!' . PHP_EOL;
-
-    $imagePath = $imageProperties->file_path;
-    $imageName = $imageProperties->file_name;
-    $imageExtension = $imageProperties->file_extension;
-
-    $addFilterImagesService->__invoke($imagePath, $imageName, $imageExtension, 'addBlackAndWhiteFilter');
-
-    echo 'order 66 is execute!!!' . PHP_EOL;
+        $imagePath = $imageProperties->file_path;
+        $imageName = $imageProperties->file_name;
+        $imageExtension = $imageProperties->file_extension;
+    
+        $filterImageCreate = $addFilterImagesService->__invoke($imagePath, $imageName, $imageExtension, 'addBlackAndWhiteFilter');
+        echo $filterImageCreate . PHP_EOL;
+    } catch (RuntimeException $exception) {
+        $exceptionClassToHumanMessage = new ExceptionClassToHumanMessageMapper();
+        echo $exceptionClassToHumanMessage->map(get_class($exception)) . '. Error code: ' . $exception->getCode(
+            ) . '.' . PHP_EOL;
+    }
 };
 
 $channel->basic_consume('blackAndWhite', '', false, true, false, false, $callback);
--- www/html/test-app/FlipHorizontalFilterConsumer.php
+++ www/html/test-app/FlipHorizontalFilterConsumer.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use TestApp\ImagesFilter\ApplicationService\AddFilterImagesService;
 use TestApp\ImagesFilter\Infrastructure\FilterImageCreator;
+use TestApp\Shared\Infrastructure\Exceptions\ExceptionClassToHumanMessageMapper;
 
 use function GuzzleHttp\json_decode;
 
@@ -13,25 +14,26 @@ $channel = $rabbitmq->channel();
 
 $channel->queue_declare('flipHorizontal', false, false, false, false);
 
-echo 'Add filter to images ' . PHP_EOL;
-echo 'Hello there!' . PHP_EOL;
+echo 'Add flip horizontal filter to images ' . PHP_EOL;
 
 $filterImageCreator = new FilterImageCreator();
 $addFilterImagesService = new AddFilterImagesService($filterImageCreator);
 
 $callback = function ($msg) use ($addFilterImagesService){
-    echo ' [x] Add Filter blur: ' . $msg->body . PHP_EOL;
-    $imageProperties = json_decode($msg->body);
+    try{
+        $imageProperties = json_decode($msg->body);
 
-    echo 'general kenobi!' . PHP_EOL;
-
-    $imagePath = $imageProperties->file_path;
-    $imageName = $imageProperties->file_name;
-    $imageExtension = $imageProperties->file_extension;
-
-    $addFilterImagesService->__invoke($imagePath, $imageName, $imageExtension, 'addFlipHorizontalFilter');
-
-    echo 'order 66 is execute!!!' . PHP_EOL;
+        $imagePath = $imageProperties->file_path;
+        $imageName = $imageProperties->file_name;
+        $imageExtension = $imageProperties->file_extension;
+    
+        $filterImageCreate = $addFilterImagesService->__invoke($imagePath, $imageName, $imageExtension, 'addFlipHorizontalFilter');
+        echo $filterImageCreate . PHP_EOL;
+    } catch (RuntimeException $exception) {
+        $exceptionClassToHumanMessage = new ExceptionClassToHumanMessageMapper();
+        echo $exceptionClassToHumanMessage->map(get_class($exception)) . '. Error code: ' . $exception->getCode(
+            ) . '.' . PHP_EOL;
+    }
 };
 
 $channel->basic_consume('flipHorizontal', '', false, true, false, false, $callback);
