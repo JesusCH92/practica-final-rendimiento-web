59d5f141da50d7c3e97a5547fd7cb4f43766029b
JesusCH92 <jcocaharo@hotmail.com>
2020-06-22 12:29:14 +0200

    update->inject by construct EventDispatcher symfony's library for dispatch ImageCreateDomainEvent DTO

--- www/html/test-app/src/ImagesFilter/ApplicationService/AddFilterImagesService.php
+++ www/html/test-app/src/ImagesFilter/ApplicationService/AddFilterImagesService.php
@@ -2,18 +2,26 @@
 
 namespace TestApp\ImagesFilter\ApplicationService;
 
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use TestApp\ImageSaver\Domain\ImageCreateDomainEvent;
 use TestApp\ImagesFilter\Domain\FilterRepository;
 
 class AddFilterImagesService
 {
     private FilterRepository $filterRepository;
-    public function __construct(FilterRepository $filterRepository)
+    public function __construct(FilterRepository $filterRepository, EventDispatcher $eventDispatcher)
     {
         $this->filterRepository = $filterRepository;
+        $this->eventDispatcher = $eventDispatcher;
     }
     public function __invoke(string $imagePath, string $imageName, string $imageExtension, string $filterAdded)
     {
-        $isFilterImageCreate = $this->filterRepository->createFilterImage($imagePath, $imageName, $imageExtension, $filterAdded);
-        return $isFilterImageCreate;
+        $filterImageCreate = $this->filterRepository->createFilterImage($imagePath, $imageName, $imageExtension, $filterAdded);
+
+        $imageCreateDomainEvent = new ImageCreateDomainEvent($imagePath, "$imageName-$filterAdded", "png");
+
+        $this->eventDispatcher->dispatch($imageCreateDomainEvent, ImageCreateDomainEvent::EVENTNAME);
+
+        return $filterImageCreate;
     }
 }
