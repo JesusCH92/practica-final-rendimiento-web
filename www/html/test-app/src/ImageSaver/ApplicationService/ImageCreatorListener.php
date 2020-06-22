84bd5e09823293bd167c0fafc7037d8c96ce1410
JesusCH92 <jcocaharo@hotmail.com>
2020-06-22 12:17:16 +0200

    create service for save images with filters in Database ->it's not finished

---
+++ www/html/test-app/src/ImageSaver/ApplicationService/ImageCreatorListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace TestApp\ImageSaver\ApplicationService;
+
+use TestApp\ImageSaver\Domain\ImageCreateDomainEvent;
+use TestApp\ImageSaver\Domain\ImageRepository;
+
+class ImageCreatorListener
+{
+    private ImageRepository $imageRespository;
+
+    public function __construct(ImageRepository $imageRespository)
+    {
+        $this->imageRespository = $imageRespository;
+    }
+
+    public function imageCreator(ImageCreateDomainEvent $event)
+    {
+        var_dump($event);
+        $imagePath = $event->imagePath();
+        $imageName = $event->imageName();
+        $imageExtension = $event->imageExtension();
+        
+        $image = "$imagePath/$imageName.$imageExtension";
+
+        $this->imageRespository->checkIfExistImage($image);
+
+        $this->imageRespository->imageSaveInDB();
+    }
+}
