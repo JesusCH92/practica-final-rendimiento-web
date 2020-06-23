6798c62fe4ddd890b6aaf52c6bd55fae9ae87996
JesusCH92 <jcocaharo@hotmail.com>
2020-06-23 12:47:20 +0200

    implement the methods: isImageInRedis and imageSavedInRedis, other methods are missing

--- www/html/test-app/src/ImageSaver/Infrastructure/ImageInDatabase.php
+++ www/html/test-app/src/ImageSaver/Infrastructure/ImageInDatabase.php
@@ -8,17 +8,40 @@ use TestApp\Shared\Infrastructure\ImageDBConnector;
 class ImageInDatabase implements ImageRepository
 {
     private ImageDBConnector $imageDBConnector;
+
     public function __construct(ImageDBConnector $imageDBConnector)
     {
         $this->imageDBConnector = $imageDBConnector;
     }
-    public function checkIfExistImage(string $image)
+
+    public function isImageInRedis(string $image)
     {
-        echo "$image esta es una imagen molona" . PHP_EOL;
+        $isImageInRedis = $this->imageDBConnector->redis()->exists($image) === 1;
+        return  $isImageInRedis;
     }
 
-    public function imageSaveInDB()
+    public function imageSavedInRedis(string $image, string $tag, string $imageDescription)
+    {
+        $imageMetadata = array(
+            'tag' => array($tag),
+            'description' => $imageDescription
+        );
+
+        $this->imageDBConnector->redis()->set($image,json_encode($imageMetadata));
+    }
+
+    public function imageSavedInMySQL(string $image)
     {
         echo 'aqui guardaremos la imagen: falta implementarlo' . PHP_EOL;
+        echo 'esto ha guardado redis de la image: ' . PHP_EOL;
+        var_dump(   $this->imageDBConnector->redis()->get($image)  );
+        echo 'Estas son todas las keys en Redis: ' . PHP_EOL;
+        var_dump(   $this->imageDBConnector->redis()->keys('*')  );
+
+        if (count($this->imageDBConnector->redis()->keys('*') > 12)){
+            echo 'Borrar todas las keys en Redis: ' . PHP_EOL;
+            $this->imageDBConnector->redis()->delete($this->imageDBConnector->redis()->keys('*'));
+        }
+        
     }
 }
