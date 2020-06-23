99b42c378f4ae791f964169700bb0d5eabf9e70e
JesusCH92 <jcocaharo@hotmail.com>
2020-06-23 14:45:16 +0200

    add isImageInDB for check if exist image in db

--- www/html/test-app/src/ImageSaver/Domain/ImageRepository.php
+++ www/html/test-app/src/ImageSaver/Domain/ImageRepository.php
@@ -6,5 +6,6 @@ interface ImageRepository
 {
     public function isImageInRedis(string $image);
     public function imageSavedInRedis(string $image, string $tag, string $imageDescription);
+    public function isImageInDB(string $imageName);
     public function imageSavedInMySQL(string $image);
 }
