adab6519f3d06e9b00780c656a1668cba31a2555
JesusCH92 <jcocaharo@hotmail.com>
2020-06-23 12:38:52 +0200

    add Redis service in ImageDBConnector, this will work like cache

--- www/html/test-app/src/Shared/Infrastructure/ImageDBConnector.php
+++ www/html/test-app/src/Shared/Infrastructure/ImageDBConnector.php
@@ -7,15 +7,22 @@ use PDO;
 class ImageDBConnector
 {
     private PDO $pdo;
+    private $redis;
 
     public function __construct()
     {
         $this->pdo = new PDO('mysql:dbname=db;host=mysql', 'user', 'password');
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->redis = new \Redis();
+        $this->redis->connect('redis');
     }
 
     public function pdo()
     {
         return $this->pdo;
+    }
+    public function redis()
+    {
+        return $this->redis;
     }
 }
