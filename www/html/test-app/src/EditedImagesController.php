90a4bcad9f7b52642d98bf0357b5b77d4f504bdb
JesusCH92 <jcocaharo@hotmail.com>
2020-06-25 00:59:14 +0200

    refactor in proccess -> change service for get images from Redis and MySQL

--- www/html/test-app/src/EditedImagesController.php
+++ www/html/test-app/src/EditedImagesController.php
@@ -3,18 +3,27 @@
 namespace TestApp;
 
 use Symfony\Component\HttpFoundation\Response;
+use TestApp\ImageEditor\ApplicationService\GetAllImagesService;
+use TestApp\ImageEditor\Infrastructure\ImagesSearcher;
 use TestApp\ImagesProcess\ApplicationService\ShowPhotoService;
 use TestApp\ImagesProcess\Infrastructure\PhotosInMemory;
+use TestApp\Shared\Infrastructure\ImageDBConnector;
 
 class EditedImagesController extends BaseController
 {
     public function __invoke()
     {
+        $imageDBConnector = new ImageDBConnector();
+        $imagesSearcher = new ImagesSearcher($imageDBConnector);
+
+        $getAllImagesService = new GetAllImagesService($imagesSearcher);
+
         $showPhotoService = new ShowPhotoService(new PhotosInMemory());        
         
         return Response::create($this->templateEngine->render('EditedImages/editedImages.html.twig', 
             [
                 'imagesCollection' => $showPhotoService(),
+                'imagesInCache' => $getAllImagesService()
             ]
         ));
     }
