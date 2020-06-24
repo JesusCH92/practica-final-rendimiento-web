9ae070a2b7ee774276038579355923e4d9f3b717
JesusCH92 <jcocaharo@hotmail.com>
2020-06-24 21:21:13 +0200

    refactor for save all mimetypes of image in DB

--- www/html/test-app/src/ImageSaver/Domain/ImageRepository.php
+++ www/html/test-app/src/ImageSaver/Domain/ImageRepository.php
@@ -4,8 +4,8 @@ namespace TestApp\ImageSaver\Domain;
 
 interface ImageRepository
 {
-    public function isImageInRedis(string $image);
-    public function imageSavedInRedis(string $image, string $tag, string $imageDescription);
-    public function isImageInDB(string $imageName);
-    public function imageSavedInMySQL(string $image, string $tags);
+    public function isImageInRedis(string $imagePath, string $imageName, string $imageExtension, string $filterAdded);
+    public function imageSavedInRedis(string $imagePath, string $imageName, string $imageExtension, string $filterAdded, string $imageDescription);
+    public function isImageInDB(string $imagePath, string $imageName, string $imageExtension, string $filterAdded);
+    public function imageSavedInMySQL(string $imagePath, string $imageName, string $imageExtension, string $filterAdded);
 }
