a4b672a318c7a665e0317e90cda93c29bf1c03b2
JesusCH92 <jcocaharo@hotmail.com>
2020-06-24 18:31:40 +0200

    add event for listen click ande delete tag

--- www/html/test-app/assets/js/EditImageModule.js
+++ www/html/test-app/assets/js/EditImageModule.js
@@ -1,3 +1,56 @@
 var EditImageModule = (function(){
     console.log('Edit images and send in ELK');
+    var $addTagBtn = $(".add-tag-btn");
+    var $inputTag = "#input-tag-";
+    var $addTagContainer = "#tags-container-";
+    var $deleteTagBtn = $(".delete-tag");
+    var $tagContainer = $(".tags-container");
+
+
+    var cleanTagInput = function($idTag){
+        var $tagInput = $($inputTag + $idTag);
+        $tagInput.val("");
+    }
+
+    var paintTag = function($idTagBtn){
+        var $tagInput = $($inputTag + $idTagBtn).val();
+        var $tagContainer = $($addTagContainer + $idTagBtn);
+
+        var $tagAdded = '<span class="badge badge-light"></span>';
+        $tagContainer.append($tagAdded);
+
+        $tagContainer.children().last().text($tagInput);
+        $tagContainer.children().last().append('<div class="delete-tag"></div>')
+        console.log($tagInput);
+    }
+
+
+    var initEvents = function(){
+
+        $addTagBtn.click(function(){
+            var $idTagBtn = $(this).attr('id');
+            var $idTag = $idTagBtn.replace("add-tag-btn-", "");
+
+            if ($($inputTag + $idTag).val() === "") {
+                return;
+            }
+            paintTag($idTag);
+
+            cleanTagInput($idTag);
+        });
+
+        $tagContainer.click(function(){
+            if ( !$(event.target).hasClass('delete-tag')) {
+                return;
+            }
+            console.log('quieres eliminar este tag?');
+            var $tagToDelete = $(event.target).parent();
+            var $textTagToDelete = $.trim($tagToDelete.text());
+            console.log($textTagToDelete);
+            $tagToDelete.remove();
+        });
+
+    }
+
+    initEvents();
 })()
