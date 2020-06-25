b5c581e35cfb8ce2774d3bd0b69a61f6fb853db6
JesusCH92 <jcocaharo@hotmail.com>
2020-06-25 15:10:59 +0200

    refactor->change ShowPhotoService(in memory) for GetAllImagesService (in DB)

--- www/html/test-app/src/EditedImagesController.php
+++ www/html/test-app/src/EditedImagesController.php
@@ -5,8 +5,6 @@ namespace TestApp;
 use Symfony\Component\HttpFoundation\Response;
 use TestApp\ImageEditor\ApplicationService\GetAllImagesService;
 use TestApp\ImageEditor\Infrastructure\ImagesSearcher;
-use TestApp\ImagesProcess\ApplicationService\ShowPhotoService;
-use TestApp\ImagesProcess\Infrastructure\PhotosInMemory;
 use TestApp\Shared\Infrastructure\ImageDBConnector;
 
 class EditedImagesController extends BaseController
@@ -18,12 +16,11 @@ class EditedImagesController extends BaseController
 
         $getAllImagesService = new GetAllImagesService($imagesSearcher);
 
-        // $showPhotoService = new ShowPhotoService(new PhotosInMemory());        
+        $imageCollection = $getAllImagesService();         
         
         return Response::create($this->templateEngine->render('EditedImages/editedImages.html.twig', 
             [
-                // 'imagesCollection' => $showPhotoService(),
-                'imagesCollection' => $getAllImagesService()
+                'imagesCollection' => $imageCollection
             ]
         ));
     }
