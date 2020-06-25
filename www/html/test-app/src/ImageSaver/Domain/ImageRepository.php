424de1349b1ab489880b443584db00046c5f8551
JesusCH92 <jcocaharo@hotmail.com>
2020-06-25 02:51:29 +0200

    refactor->update methods for save uuid like rename image

--- www/html/test-app/src/ImageSaver/Domain/ImageRepository.php
+++ www/html/test-app/src/ImageSaver/Domain/ImageRepository.php
@@ -4,8 +4,8 @@ namespace TestApp\ImageSaver\Domain;
 
 interface ImageRepository
 {
-    public function isImageInRedis(string $imagePath, string $imageName, string $imageExtension, string $filterAdded);
-    public function imageSavedInRedis(string $imagePath, string $imageName, string $imageExtension, string $filterAdded, string $imageDescription);
-    public function isImageInDB(string $imagePath, string $imageName, string $imageExtension, string $filterAdded);
-    public function imageSavedInMySQL(string $imagePath, string $imageName, string $imageExtension, string $filterAdded);
+    public function isImageInRedis(string $imageRename);
+    public function imageSavedInRedis(string $imagePath, string $imageName, string $imageRename, string $imageExtension, string $filterAdded, string $imageDescription);
+    public function isImageInDB(string $imageRename);
+    public function imageSavedInMySQL(string $imagePath, string $imageName, string $imageRename, string $imageExtension, string $filterAdded);
 }
