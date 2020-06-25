4331da11bdbaf27e3c510299bf3622c1f6937238
JesusCH92 <jcocaharo@hotmail.com>
2020-06-25 21:25:31 +0200

    add TagCreatorController with decorator patron

---
+++ www/html/test-app/src/TagCreatorController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace TestApp;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use TestApp\ImageDecorator\Infrastructure\ImageInMySql;
+use TestApp\ImageDecorator\Infrastructure\ImageInRedis;
+use TestApp\ImageDecorator\Infrastructure\TagCreatorToImage;
+use TestApp\Shared\Infrastructure\ImageDBConnector;
+
+class TagCreatorController extends BaseController
+{
+    public function __invoke(Request $request)
+    {
+        echo 'General Kenobi!!' . PHP_EOL;
+
+        if (!$request->isXmlHttpRequest()) {
+            throw new \Exception('Error type request!!!');
+        }
+
+        $imageName = $request->get('imageName');
+        $tagText = $request->get('tag');
+
+        var_dump($tagText);
+        var_dump($imageName);
+        var_dump($request->request->all());
+
+        $imageDBConnector = new ImageDBConnector();
+
+        $imageInRedis = new ImageInRedis($imageDBConnector);
+        $imageInMySql = new ImageInMySql($imageDBConnector);
+
+        $tagCreatorToImage = new TagCreatorToImage($imageInRedis, $imageInMySql);
+
+        if ($tagCreatorToImage->getImageDetails($imageName) === null) {
+            echo 'ERROR_IMAGE_NOT_FOUND' . PHP_EOL;
+            return;
+        }
+        $imageDetails = $tagCreatorToImage->getImageDetails($imageName);
+        $tagCreatorToImage->createTag($imageName, $imageDetails, $tagText);
+
+        return new JsonResponse(['Edited Image - new tag']);    
+    }
+}
