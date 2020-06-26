4e9203611f1423120d3731b6dc9c0a9eab06b55c
JesusCH92 <jcocaharo@hotmail.com>
2020-06-26 22:01:29 +0200

    add elasticsearch dependency

--- www/html/test-app/src/Shared/Infrastructure/ImageDBConnector.php
+++ www/html/test-app/src/Shared/Infrastructure/ImageDBConnector.php
@@ -3,11 +3,15 @@
 namespace TestApp\Shared\Infrastructure;
 
 use PDO;
+use Elasticsearch\ClientBuilder;
 
 class ImageDBConnector
 {
     private PDO $pdo;
     private $redis;
+    private $elasticsearch;
+
+    const INDEXNAME = 'imagesmpwar';
 
     public function __construct()
     {
@@ -15,14 +19,52 @@ class ImageDBConnector
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         $this->redis = new \Redis();
         $this->redis->connect('redis');
+        $this->elasticsearch = ClientBuilder::create()->setHosts(["elasticsearch:9200"])->build();
+        $params = [
+            'index' => self::INDEXNAME,
+            'body' => [
+                'mappings' => [
+                    'properties' => [
+                        'image_path' => [
+                            'type' => 'text'
+                        ],
+                        'image_name' => [
+                            'type' => 'text'
+                        ],
+                        'image_rename' => [
+                            'type' => 'text'
+                        ],
+                        'image_extension' => [
+                            'type' => 'text'
+                        ],
+                        'tags' => [
+                            'type' => 'keyword'
+                        ],
+                        'description' => [
+                            'type' => 'text'
+                        ],
+                    ]
+                ]
+            ]
+        ];
+        if (!$this->elasticsearch->indices()->exists(['index' => self::INDEXNAME])){
+            $this->elasticsearch->indices()->create($params);
+        }
     }
 
     public function pdo()
     {
         return $this->pdo;
     }
+
     public function redis()
     {
         return $this->redis;
     }
+
+    public function elasticsearch()
+    {
+        return $this->elasticsearch;
+    }
+
 }
